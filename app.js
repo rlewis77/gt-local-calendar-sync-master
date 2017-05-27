bb38e82d24f72ac2bb1de94ba6a4b65084809e87
rylew2 <rlewis77@gatech.edu>
2017-05-26 17:55:48 -0700

    Add api stuff

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
+var google = require('googleapis');
+var dotenv = require('dotenv').config();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+// app.get('/', function (req, res) {
+//     res.send('R   World');
+// });
+
+
+
+app.get('/calendar-events', function (req, res) {
+    res.send('MfdY Events!');
 });
+
+
+app.use('/', express.static('public'));
+
 
 app.listen(3007, function () {
     console.log('Example app listening on port 3007!');
     console.log('http://localhost:3007/');
 });
+
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
+
+/**
+ * Gulp Tasks
+ */
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync({
+        proxy: "localhost:3007",  // local node app address
+        port: 5000,  // use *different* port than above
+        notify: true,
+        browser: ["chrome"]
+    });
+});
+
+gulp.task('nodemon', function (cb) {
+    var called = false;
+    return nodemon({
+        script: 'app.js',
+        ignore: [
+            'gulpfile.js',
+            'node_modules/'
+        ]
+    })
+        .on('start', function () {
+            if (!called) {
+                called = true;
+                cb();
+            }
+        });
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch(['*.*'], reload);
+    gulp.watch(['public/*.*'], reload);
+});
